8405386e92a017ee4ee33fe468b0f41e1a98c21f
ichiadrian <ichiiadriian@gmail.com>
2020-10-29 02:59:38 +0700

    new

--- nested-loop.js
+++ nested-loop.js
@@ -1,11 +1,11 @@
 function data(){
 
   for (var i = 0; i<2; i++) {
-    console.log("data dari", i)
+    console.log("data daru", i)
     for (var j = 0; j<2; j++) {
       console.log("data dari", j)
     }
-    console.log("");
+    console.log("------------------------------------------");
   }
 
 }
