88ef7813972a99db81eab5c96dc460bf86ab939f
ichiadrian <ichiiadriian@gmail.com>
2020-10-30 21:40:16 +0700

    update new

---
+++ compare.js
@@ -0,0 +1,10 @@
+function say(message) {
+	if (!message) {
+		console.log('undifine')
+	} else {
+		console.log(message+" "+"haha"+"pea")
+	}
+}
+
+say('dodol aja lu')
+// module.exports = say;
---
+++ datatype.js
@@ -0,0 +1,14 @@
+let a = {
+	key1: 1,
+	key2: 2	
+};
+//data Array object
+let b = [1,2,3,4];
+
+// console.log(a);
+console.log(b);
+
+//mencari keys
+console.log(Object.keys(a));
+//mecari data valuenya
+console.log(Object.values(a));
