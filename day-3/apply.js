2d00ef27c30da2732857eee5d95a13e98958d77b
ichiadrian <ichiiadriian@gmail.com>
2020-11-22 03:10:17 +0700

    update daya days 3

--- day-3/apply.js
+++ day-3/apply.js
@@ -1,26 +1,9 @@
-// menggunakan fungsi apply pada function
+// function getLoadingMessage(isLoading) {
+//   return `Data is ${isLoading ? `loading...` : `done!`}`;
+// }
 
-var person1 = { firstName: "Tom",
-				 lastName: "Cat" 
-			};
-var person2 = { firstName: "Brown",
-				 lastName: "Sugar" 
-			};
-
-function say(greeting, greeting2="") {
-  // function memiliki environment variabe this yg bisa di assign sekaligus dijalankan menggunakan apply
-  console.log(this);
-  console.log(
-    greeting + " " + this.firstName + " " + this.lastName + " " + greeting2
-  );
+function getLoadingMessage(isLoading) {
+  return `Data is ${isLoading ? 'loading...' : 'done!'}`;
 }
 
-// execute say dengan fungsi apply, 
-// menggunakan satu input variable yaitu greeting = "Helo", sedangkan greeting2 menggunakan default value
-say.apply(person1, ["Hello"]);
-
-// execute say dengan fungsi apply, 
-// menggunakan satu input variable yaitu greeting = "Helo", dan greeting2 = "are you well?"
-say.apply(person2, ["Hello", "are you well?"]);
-
-// perbedaan dengan call terletak pada input varible diletakkan didalam array
+console.log(getLoadingMessage());
